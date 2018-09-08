23ebf475791a67e04d6181ead71da1f04701978f
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-08 13:50:26 +0530

    use strict

--- FCC/JavaScript/ES6/readOnlyVar.js
+++ FCC/JavaScript/ES6/readOnlyVar.js
@@ -1,5 +1,7 @@
 //const <variable_name>
 //common practice : uppercase naming and words seperated by an underscore.
+"use strict";
+//"use strict - enables strict mode, which catches common coding mistakes and unsafe actions."
 
 const LIST = [1, 2, 3];
 
