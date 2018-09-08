63a839f757d975c4165aed6f79494f75587324e0
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-08 14:31:17 +0530

    Arrow Function

---
+++ FCC/JavaScript/ES6/arrowFunction.js
@@ -0,0 +1,14 @@
+// Arrow Function
+
+const myFunc = () => {
+    const myVar = "value";
+    return myVar;
+}
+
+//Arrow function syntax allows us to omit keyword "return"
+//We can write earlier function like this
+//If the function is one line, we do not need to put { }
+const myfun = () => "value";
+
+//Arrow function with parameter
+const doubler = (item) => item * 2;
