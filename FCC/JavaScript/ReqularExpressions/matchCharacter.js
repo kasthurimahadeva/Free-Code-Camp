11dd7eb0121ab8d92a0543e70b5527cb397b85d9
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 19:03:05 +0530

    Match Everything But Letters and Numbers

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -135,3 +135,16 @@ shortHand.test(varNames); // Returns true
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/g; // Change this line
 let result = quoteSample.match(alphabetRegexV2).length;
+
+// Match Everything But Letters and Numbers
+// opposite of the \w with \W
+// This shortcut is the same as [^A-Za-z0-9_]
+let shortHand = /\W/;
+let numbers = "42%";
+let sentence = "Coding!";
+numbers.match(shortHand); // Returns ["%"]
+sentence.match(shortHand); // Returns ["!"]
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
