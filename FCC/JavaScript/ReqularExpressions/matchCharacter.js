0c45dfddbdb574397b06e72a50c9b4640a39a577
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 13:45:22 +0530

    Ignore Case While Matching

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -12,3 +12,8 @@ let result = waldoRegex.test(waldoIsHiding);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
+
+//Ignore Case While Matching
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let result = fccRegex.test(myString);
