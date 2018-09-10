5b631aed10c8eb871166cbfe6a1e2c6836661028
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 13:42:03 +0530

    Match a Literal String with Different Possibilities

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -7,3 +7,8 @@ let result = myRegex.test(myString);
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; 
 let result = waldoRegex.test(waldoIsHiding);
+
+// Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
