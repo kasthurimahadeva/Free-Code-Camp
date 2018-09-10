a4f09d1c75bfe19d08f42f7668abb8f61f2026d4
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 13:40:16 +0530

    Match Literal Strings

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -2,3 +2,8 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
+// Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; 
+let result = waldoRegex.test(waldoIsHiding);
