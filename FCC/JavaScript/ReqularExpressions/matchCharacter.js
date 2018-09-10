e4bccf4863eb1474019abf4daacef6df4442da7c
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 19:40:12 +0530

    Match Non-Whitespace Characters

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -188,3 +188,8 @@ whiteSpace.match(spaceRegex);
 let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
+
+// Match Non-Whitespace Characters
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
