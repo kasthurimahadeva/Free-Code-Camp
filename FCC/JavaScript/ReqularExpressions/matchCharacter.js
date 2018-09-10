57a8082f7baaab6533917e7d1ba55acceea4fa09
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-10 14:20:21 +0530

    Match Letters of the Alphabet

--- FCC/JavaScript/ReqularExpressions/matchCharacter.js
+++ FCC/JavaScript/ReqularExpressions/matchCharacter.js
@@ -37,3 +37,16 @@ let result = unRegex.test(exampleStr);
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi;
 let result = quoteSample.match(vowelRegex);
+
+//Match Letters of the Alphabet
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex); // Returns ["cat"]
+batStr.match(bgRegex); // Returns ["bat"]
+matStr.match(bgRegex); // Returns null
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let result = quoteSample.match(alphabetRegex); 
