6319279d247afd42e1f179e78944a9436cc2319c
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-13 11:57:24 +0530

    Use typeof to Check the Type of a Variable

---
+++ FCC/JavaScript/Debugging /debugging.js
@@ -0,0 +1,8 @@
+// Use typeof to Check the Type of a Variable
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+console.log(typeof seven);
+console.log(typeof three);
