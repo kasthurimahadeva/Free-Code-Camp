a9a94fa91bc1992de2a1988de5270ae1e206bf72
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-13 23:08:09 +0530

    Add Items Using splice()

--- FCC/JavaScript/BasicDataStructures/basicDataStructure.js
+++ FCC/JavaScript/BasicDataStructures/basicDataStructure.js
@@ -69,3 +69,19 @@ function sumOfTen(arr) {
   
   // do not change code below this line
   console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
+
+
+// Add Items Using splice()
+/*We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. 
+Modify the function using splice() to remove the first two elements of the array and add 
+'DarkSalmon' and 'BlanchedAlmond' in their respective places.*/
+
+function htmlColorNames(arr) {
+    // change code below this line
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
+    // change code above this line
+    return arr;
+  } 
+   
+  // do not change code below this line
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
--- FCC/JavaScript/BasicDataStructures/basicDataStructures.md
+++ FCC/JavaScript/BasicDataStructures/basicDataStructures.md
@@ -71,3 +71,19 @@ array.splice(2, 2);
 // remove 2 elements beginning with the 3rd element
 // array now equals ['today', 'was', 'great']
 ```
+
+<!--- Add Items Using splice()--->
+* third parameter : one or more elements, to *add*  them as well
+
+```javascript
+function colorChange(arr, index, newColor) {
+  arr.splice(index, 1, newColor);
+  return arr;
+}
+
+let colorScheme = ['#878787', '#a08794', '#bb7e8c', '#c9b6be', '#d1becf'];
+
+colorScheme = colorChange(colorScheme, 2, '#332327');
+// we have removed '#bb7e8c' and added '#332327' in its place
+// colorScheme now equals ['#878787', '#a08794', '#332327', '#c9b6be', '#d1becf']
+```
