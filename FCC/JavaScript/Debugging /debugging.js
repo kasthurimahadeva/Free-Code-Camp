3a4442c8fcb5073924b70945d85095f75bb503f8
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-13 12:00:43 +0530

    Catch Misspelled Variable and Function Names

--- FCC/JavaScript/Debugging /debugging.js
+++ FCC/JavaScript/Debugging /debugging.js
@@ -6,3 +6,17 @@ console.log(seven + three);
 // Add your code below this line
 console.log(typeof seven);
 console.log(typeof three);
+
+// Catch Misspelled Variable and Function Names
+
+//before
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//after
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
