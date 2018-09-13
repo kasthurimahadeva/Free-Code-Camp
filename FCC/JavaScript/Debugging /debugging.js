75b9c701bee0ed886a8d4e4cff5e304791546acd
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-13 12:03:43 +0530

    Catch Unclosed Parentheses, Brackets, Braces and Quotes

--- FCC/JavaScript/Debugging /debugging.js
+++ FCC/JavaScript/Debugging /debugging.js
@@ -20,3 +20,16 @@ let receivables = 10;
 let payables = 8;
 let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
+
+
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+//before
+// let myArray = [1, 2, 3;
+//     let arraySum = myArray.reduce((previous, current =>  previous + current);
+//     console.log(`Sum of array values is: ${arraySum}`);
+
+// after
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
