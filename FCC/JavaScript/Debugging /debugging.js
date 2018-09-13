facff31001cd19fd5ae39f54990ddcae01870f9b
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-13 13:44:51 +0530

    Use Caution When Reinitializing Variables Inside a Loop

--- FCC/JavaScript/Debugging /debugging.js
+++ FCC/JavaScript/Debugging /debugging.js
@@ -153,3 +153,48 @@ function countToFive() {
 }
   
 countToFive();
+
+
+// Use Caution When Reinitializing Variables Inside a Loop
+
+// before
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+      
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+}
+  
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+// after
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+      row = [];
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+}
+  
+let matrix = zeroArray(3, 2);
+console.log(matrix);
