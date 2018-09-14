ac0b0a23c965af5777cd4cea25fec9d11c7ae70c
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-14 15:47:00 +0530

    Access Property Names with Bracket Notation

--- FCC/JavaScript/BasicDataStructures/basicDataStructure.js
+++ FCC/JavaScript/BasicDataStructures/basicDataStructure.js
@@ -255,3 +255,24 @@ userActivity['data']['online'] = 45;
 // change code above this line
 
 console.log(userActivity);
+
+
+
+//Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+// do not change code above this line
+
+function checkInventory(scannedItem) {
+  // change code below this line
+  return foods[scannedItem];
+}
+
+// change code below this line to test different cases:
+console.log(checkInventory("apples"));
