a7d87aa7cd3b20456f41eb452fda6862147f22f8
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-14 15:40:42 +0530

    Modify an Object Nested Within an Object

--- FCC/JavaScript/BasicDataStructures/basicDataStructure.js
+++ FCC/JavaScript/BasicDataStructures/basicDataStructure.js
@@ -238,3 +238,20 @@ foods['strawberries'] = 27;
 // change code above this line
 
 console.log(foods);
+
+
+// Modify an Object Nested Within an Object
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// change code below this line
+userActivity['data']['online'] = 45;
+// change code above this line
+
+console.log(userActivity);
