56e62cdc566972a19508c5f6ec99d48a812f7318
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-14 16:17:47 +0530

    Generate an Array of All Object Keys with Object.keys()

--- FCC/JavaScript/BasicDataStructures/basicDataStructure.js
+++ FCC/JavaScript/BasicDataStructures/basicDataStructure.js
@@ -364,3 +364,33 @@ function countOnline(obj) {
 }
 
 console.log(countOnline(users));
+
+
+// Generate an Array of All Object Keys with Object.keys()
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+  let properties = Object.keys(obj);
+  return properties;
+  // change code above this line
+}
+
+console.log(getArrayOfUsers(users));
