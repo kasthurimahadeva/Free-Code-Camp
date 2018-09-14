06adb56c69599061a93dd2e55b8707950502c435
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-14 15:49:32 +0530

    Use the delete Keyword to Remove Object Properties

--- FCC/JavaScript/BasicDataStructures/basicDataStructure.js
+++ FCC/JavaScript/BasicDataStructures/basicDataStructure.js
@@ -276,3 +276,22 @@ function checkInventory(scannedItem) {
 
 // change code below this line to test different cases:
 console.log(checkInventory("apples"));
+
+
+// Use the delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// change code above this line
+
+console.log(foods);
