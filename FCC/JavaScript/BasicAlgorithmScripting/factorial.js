8ae07516cb9374e2b277369bdf0a417eeeb69542
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-15 05:55:26 +0530

    Shortest way to find factorial

--- FCC/JavaScript/BasicAlgorithmScripting/factorial.js
+++ FCC/JavaScript/BasicAlgorithmScripting/factorial.js
@@ -1,5 +1,5 @@
 // Factorialize a Number
-
+// my idea
 function factorialize(num) {
     let ans = 1;
     for (let i = 2; i <= num; i++){
@@ -10,3 +10,11 @@ function factorialize(num) {
   }
   
   factorialize(5);
+
+ // short way
+ function factorialize(num) {
+  if (num === 0) { return 1; }
+  return num * factorialize(num-1);
+}
+
+factorialize(5); 
