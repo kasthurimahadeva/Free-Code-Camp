39967c0af7fc5d9d28edcd40591db4c93ae7ec2f
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-16 14:51:45 +0530

    Title Case a Sentence

---
+++ FCC/JavaScript/BasicAlgorithmScripting/TitleCaseSentence.js
@@ -0,0 +1,12 @@
+// Title Case a Sentence
+// my idea
+
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+      for (var i = 0; i < str.length; i++) {
+          str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+      }
+      return str.join(' ');
+}
+  
+titleCase("I'm a little tea pot");
