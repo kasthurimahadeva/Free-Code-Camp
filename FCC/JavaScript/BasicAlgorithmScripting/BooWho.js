ab0b72a69222eb179bac1e293dd3d93e7312eda1
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-16 14:00:58 +0530

    Boo Who - some changes

--- FCC/JavaScript/BasicAlgorithmScripting/BooWho.js
+++ FCC/JavaScript/BasicAlgorithmScripting/BooWho.js
@@ -2,7 +2,7 @@
 //my idea
 function booWho(bool) {
     // What is the new fad diet for ghost developers? The Boolean.
-    return (typeof(bool) === "boolean")  ? true : false;
+    return (typeof(bool) === "boolean");
 }
   
 booWho(null);
