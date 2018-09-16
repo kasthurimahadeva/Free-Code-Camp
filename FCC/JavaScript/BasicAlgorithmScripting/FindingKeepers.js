f002ff31dca5c3516bd33a78eead22a0baab92ce
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-16 13:54:02 +0530

    short way to Finding Keepers

--- FCC/JavaScript/BasicAlgorithmScripting/FindingKeepers.js
+++ FCC/JavaScript/BasicAlgorithmScripting/FindingKeepers.js
@@ -13,3 +13,14 @@ function findElement(arr, func) {
 }
   
 findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+
+// short way
+function findElement(arr, func) {
+    let filterArr = arr.filter(func); //filter array with the function provided
+  
+    return filterArr[0]; //return the first element that returns true, or undefined if no elements return true
+}
+  
+// test here
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
