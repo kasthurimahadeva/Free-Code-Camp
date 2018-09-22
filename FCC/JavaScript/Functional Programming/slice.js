ce35c8a0400d51f9433966ef1c097600cebaa61a
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-22 13:20:54 +0530

    Remove Elements from an Array Using slice Instead of splice

--- FCC/JavaScript/Functional Programming/slice.js
+++ FCC/JavaScript/Functional Programming/slice.js
@@ -13,3 +13,25 @@ function sliceArray(anim, beginSlice, endSlice) {
   }
   var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
   sliceArray(inputAnim, 1, 3);
+
+
+
+
+// Remove Elements from an Array Using slice Instead of splice
+
+/*
+Rewrite the function nonMutatingSplice by using slice instead of splice. 
+It should limit the provided cities array to a length of 3, and return a new array 
+with only the first three items.
+
+Do not mutate the original array provided to the function.
+*/
+
+function nonMutatingSplice(cities) {
+    // Add your code below this line
+    return cities.slice(0, 3);
+    
+    // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
