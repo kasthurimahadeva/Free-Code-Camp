b6a837eae557c42bd33babc53374f25459feec10
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-22 13:11:18 +0530

    Implement the filter Method on a Prototype

--- FCC/JavaScript/Functional Programming/filter.js
+++ FCC/JavaScript/Functional Programming/filter.js
@@ -136,3 +136,32 @@ filteredList = watchList.filter(ratingFilter).map(ratingMap);
 // Add your code above this line
 
 console.log(filteredList); 
+
+
+
+
+// Implement the filter Method on a Prototype
+/*
+Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter().
+You may use a for loop or the Array.prototype.forEach() method.
+*/
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  for(let i = 0; i < this.length; i++){
+    if(callback(this[i]) === true){
+      newArray.push(this[i]);
+    }
+  }
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
