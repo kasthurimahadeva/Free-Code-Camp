c6526ba8a20769a75360564f13a6945c3b28f6a4
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-22 12:48:15 +0530

    Implement map on a Prototype

--- FCC/JavaScript/Functional Programming/map.js
+++ FCC/JavaScript/Functional Programming/map.js
@@ -135,4 +135,30 @@ var watchList = [
   
   // Add your code above this line
   
-  console.log(rating); 
+  console.log(rating);
+  
+
+  
+// Implement map on a Prototype
+/*
+Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). 
+You may use a for loop or the forEach method.
+*/
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  for (let i = 0; i < this.length; i++){
+    newArray.push(callback(this[i]));
+  }
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
