f227cd7bc7dd81ca14953b3c4ac82aaccba22a1b
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-09-29 05:34:11 +0530

    Drop it

---
+++ JavaScript/Intermidiate Algorithm Scripting/Exercise15.js
@@ -0,0 +1,23 @@
+// Drop it
+
+/*
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index)
+until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+ */
+
+function dropElements(arr, func) {
+    // Drop them elements.
+    let newArr = arr.filter(func);
+    if(newArr.length > 0){
+        arr = arr.slice(arr.indexOf(newArr[0]));
+    }
+
+    else{
+        arr = [];
+    }
+    return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
